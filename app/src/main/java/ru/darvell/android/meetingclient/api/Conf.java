b07767cf8d14280dee33924d21f621676a80cc16
andrewdarvell <andrew@darvell.ru>
2015-01-23 17:52:03 +0300

    + vk libs

--- app/src/main/java/ru/darvell/android/meetingclient/api/Conf.java
+++ app/src/main/java/ru/darvell/android/meetingclient/api/Conf.java
@@ -1,14 +1,13 @@
 package ru.darvell.android.meetingclient.api;
 
 /**
- * Created by darvell on 16.01.15.
+ * Конфиг файл в памяти. Посмотрим что с ним сделать дальше
  */
 public class Conf {
 
 	public static final String apiUrl = "http://env-5340213.jelastic.regruhosting.ru/meeting/";
 	public static final String apiKey = "bcbe3365e6ac95ea2c0343a2395834dd";
 	public static String sessKey = "";
-//	public static String sessKey = "";
 
 	public static boolean exist = false;
 	public static String login = "";
